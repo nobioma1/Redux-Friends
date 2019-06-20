328e0a0caa5641e9d8d95b9e03b0b156410a3ce7
Noble Obioma <nobioma1@gmail.com>
2019-06-20 19:16:12 +0100

    Create Routes and Use PrivateRoute

--- friends/src/components/App/index.js
+++ friends/src/components/App/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter,
+} from 'react-router-dom';
 import FriendList from '../FriendsList';
 import Header from '../Header';
 import NotFound from '../NotFound';
+import LoginForm from '../LoginForm';
+import PrivateRoute from '../PrivateRoute';
+import AddFriend from '../AddFriend';
+
+const withRouterLoginForm = withRouter(LoginForm);
+const withRouterAddForm = withRouter(AddFriend);
+const withRouterNotFound = withRouter(NotFound);
 
 function App() {
   return (
@@ -10,9 +22,10 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route exact path="/" component={FriendList} />
-          <Route path="/login" />
-          <Route render={props => <NotFound {...props} />} />
+          <Route path="/login" component={withRouterLoginForm} />} />
+          <PrivateRoute exact path="/" component={FriendList} />
+          <PrivateRoute exact path="/add" component={withRouterAddForm} />
+          <Route component={withRouterNotFound} />
         </Switch>
       </Router>
     </div>
