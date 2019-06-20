2ceea6a1bcd2c4ccc352c76f2993f06f4735822d
Noble Obioma <nobioma1@gmail.com>
2019-06-20 12:04:17 +0100

    Initial setup commit - create react app friends, File Structure install dependencies redux, react-redux, redux-thunk, redux-logger, axios, react-router-dom, semantic-ui

---
+++ friends/src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div className="App">
+      <Container>App</Container>
+    </div>
+  );
+}
+
+export default App;
